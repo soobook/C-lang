d17fbcc020e693f342bc414cb9568c850785f6b9
soobook <158715034+soobook@users.noreply.github.com>
2025-12-01 09:37:30 +0900

    Add files via upload

--- code/12/ptr.c
+++ code/12/ptr.c
@@ -6,7 +6,7 @@ int main(void)
 	int* ptrint;
 	ptrint = &data;
 
-	printf("º¯¼ö¸í  ÁÖ¼Ò°ª            ÀúÀå°ª\n");
+	printf("ë³€ìˆ˜ëª…  ì£¼ì†Œê°’            ì €ìž¥ê°’\n");
 	printf("------------------------------------------\n");
 	printf("  data  %p  %d\n", &data, data);
 	printf("ptrint  %p  %p\n", &ptrint, ptrint);
